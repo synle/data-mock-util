9407928812876bca3ae4606effd73188b0051d04
Sy Le <lenguyensy@gmail.com>
2017-12-01 08:03:36 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,53 @@
+const moment = require('moment');
+
 // funcs
-function getRandomItem(list) {
+const getRandomItem = function getRandomItem(list) {
     return list[Math.floor(Math.random() * (list.length -1))];
 }
 
-function getRandomPosInteger(max) {
+const getRandomPosInteger = function getRandomPosInteger(max) {
     return getRandomFromRange(1, max);
 }
 
-function getRandomFromRange(min, max) {
+const getRandomFromRange = function getRandomFromRange(min, max) {
     return Math.floor(Math.random() * (max - min) + min);
 }
 
-function getRandomPhoneNumber(){
-    var resp = '';
-    for (var i = 0; i < 10; i++){
+const getRandomPhoneNumber = function getRandomPhoneNumber(){
+    let resp = '';
+    for (let i = 0; i < 10; i++){
+        resp += getRandomFromRange(0,9)
+    }
+    return resp;
+}
+
+const getRandomZipCode = function getRandomZipCode(){
+    let resp = '';
+    for (let i = 0; i < 5; i++){
         resp += getRandomFromRange(0,9)
     }
     return resp;
 }
 
 
+const getRandomDateObject = function getRandomDateObject(dayDelta, hourDelta){
+    hourDelta = hourDelta || 24;
+    dayDelta = dayDelta || 30;
+
+    const diffDay = getRandomPosInteger(dayDelta);
+    const diffHour = getRandomPosInteger(hourDelta)
+
+    return moment()
+        .startOf('day')
+        .add(dayDelta, 'day')
+        .add(hourDelta, 'hour');
+}
+
 module.exports = {
     getRandomItem,
     getRandomPosInteger,
     getRandomFromRange,
     getRandomPhoneNumber,
+    getRandomZipCode,
+    getRandomDateObject,
 }
